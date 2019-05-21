4b858acc19388b41fab5abcb6a4f80f4c82447b1
Justyna Wojtczak <justine84@gmail.com>
2019-05-21 21:32:18 +0200

    Add config store

--- app/controllers/v1/things_controller.rb
+++ app/controllers/v1/things_controller.rb
@@ -5,6 +5,10 @@ class V1::ThingsController < ApplicationController
             {
                 name: 'some-thing',
                 guid: '123-232-4342353432423'
+            },
+            {
+                name: 'some-thing-else',
+                guid: '123-232-43423534324234'
             }
         ]
     }.to_json
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,19 +1,24 @@
 import React from "react"
-import PropTypes from "prop-types"
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+//import PropTypes from "prop-types"
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import HelloWorld from './HelloWorld'
+import configureStore from '../configureStore'
+
+const store = configureStore();
 
 class App extends React.Component {
     render() {
         return (
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/" render={() => ("Home")}/>
-                    <Route path="/hello" render={() => <HelloWorld greeting="Friend"/> } />
-                </Switch>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" render={() => ("Home")}/>
+                        <Route path="/hello" render={() => <HelloWorld greeting="Friend"/> } />
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
         );
     }
 }
-
-export default App
+export default App;
