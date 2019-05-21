1e322899ea528686144c291c6a347844a232cb38
Justyna Wojtczak <justine84@gmail.com>
2019-05-21 16:11:48 +0200

    Add App component

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'static/index'
-  root 'static#index'
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
+
+  get '*pages', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'static#index'
 end
